1cbc29b4728d8348f9e777e83b6c33fabebb9dc9
kseniya <kseniyamudrakovadev@gmail.com>
2021-12-05 22:56:03 +0100

    change login front

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { Card } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
 import { Grid } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { Spacer } from "@nextui-org/react";
+import { Text } from "@nextui-org/react";
+import Axios from "axios";
 
 import "./pages.css";
 
 function Login() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [usernamelog, setUsernameLog] = useState("");
+  const [userpasswordlog, setUserpasswordLog] = useState("");
+
+  const [LogStat, setLogStat] = useState("");
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: usernamelog,
+      password: userpasswordlog,
+    }).then((response) => {
+      if (response.data.loggedIn) {
+        localStorage.setItem("Connecté", true);
+        localStorage.setItem("Pseudo", response.data.username);
+      } else {
+        console.log(response.data.message);
+      }
+    });
+  };
 
   return (
     <div className="wrapcontainer">
-      <Card className="Card" width="60%">
-        <h1> Groupomania </h1> <h2> Se connecter </h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <Card className="Card login" width="60%">
+        <Text h1> Groupomania </Text> <Text h2> Se connecter </Text>
+        <form>
           <Input
             rounded
             bordered
+            className="form-control"
+            type="text"
             label="Pseudo"
             placeholder="Entrez un pseudo"
+            name="username"
+            onChange={(e) => {
+              setUsernameLog(e.target.value);
+            }}
           />
-          <Spacer y={1} />{" "}
+          <Spacer y={1} />
           <Grid>
             <Input
               rounded
@@ -37,18 +55,17 @@ function Login() {
               label="Mot de passe"
               type="password"
               placeholder="Entrez un mot de passe"
+              onChange={(e) => {
+                setUserpasswordLog(e.target.value);
+              }}
             />
-          </Grid>{" "}
-        </form>{" "}
-        <Button
-          className="validationbutton"
-          size="large"
-          shadow
-          color="warning"
-        >
-          Se connecter{" "}
-        </Button>{" "}
-      </Card>{" "}
+          </Grid>
+          <Spacer y={1} />
+          <Spacer y={1} />
+          <Button onClick={login}>Se connecter</Button>
+        </form>
+        <p>{LogStat}</p>
+      </Card>
     </div>
   );
 }
