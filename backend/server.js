72f6604dafc3d873aa919e59f57f7bbab856717a
kseniya <kseniyamudrakovadev@gmail.com>
2021-12-05 22:54:39 +0100

    add register and login backend

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,43 @@ sequelize.sync()
 
 app.use(cors());
 
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
+/* const userRoute = require('./routes/User');
+app.use('/user', userRoute); */
+
+app.post('/register', async (req, res) => {
+
+    const username = req.body.username;
+    const useremail = req.body.useremail;
+    const password = req.body.password;
+
+    db.query("INSERT INTO `User` (username, useremail, password) VALUES (?, ?, ?)", [username, useremail, password], (err, results) => {
+            console.log(err);
+        }),
+        (err, results) => {
+            console.log(err);
+            res.send(results);
+        }
+});
+
+app.post('/login', async (req, res) => {
+    const username = req.body.userlogin;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT `username`, `password` FROM User", [username, password],
+        (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            if (results) {
+                if (password == results[0].password) {
+                    res.send('Connection réussie');
+                } else {
+                    res.send("Erreur dans les indentifiants");
+                }
+            }
+        })
+})
 
 app.listen(3001, (req, res) => {
     console.log("The server is running");
