71651d900bfa393c9f7399a2737c8c7c0f501540
kseniya <kseniyamudrakovadev@gmail.com>
2021-12-24 12:32:35 +0100

    progression server

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const app = express();
 
+const sequelize = require('sequelize');
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,7 @@ const http = require('http');
 const app = require('./app');
 const sequelize = require('./config/database');
 const User = require('./models/user.model');
+
+sequelize.sequelize.sync().then((req) => {
+
+})
