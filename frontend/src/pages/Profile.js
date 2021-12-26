fe17e882fa6b16ba8e187b3c1f00f9d6434e0993
kseniya <kseniyamudrakovadev@gmail.com>
2021-12-26 20:36:59 +0100

    change front

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 import { Card } from "@nextui-org/react";
-
+import { Input } from "@nextui-org/react";
 import { Grid } from "@nextui-org/react";
+import { Spacer } from "@nextui-org/react";
+import { Text } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { Avatar } from "@nextui-org/react";
 
 import "./pages.css";
 
 function Profile() {
+  const profile = () => {
+    Axios.get(
+      `http://localhost:3001/user/${localStorage.getItem("username")}`
+    ).then((response) => {});
+  };
+
   return (
     <div className="wrapcontainer">
-      <Card width="400px" bordered>
-        <Grid>
-          <Avatar
-            size="large"
-            src="/avatars/avatar-4.png"
-            color="warning"
-            bordered
-          />
-        </Grid>
-        <p>A bordered card.</p>
+      <Card className="Card" width="60%">
+        <Text h1> Votre profile </Text>
+        <Grid.Container gap={2}>
+          <Grid>
+            <Text size="1.25rem">{localStorage.getItem("username")}</Text>
+            <Avatar src="/Assets/random.png" bordered size={90} />
+            <Text size={14}>Biographie</Text>
+            <Text size={14}>Email</Text>
+          </Grid>
+        </Grid.Container>
       </Card>
     </div>
   );
