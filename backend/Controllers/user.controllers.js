0f60f0fdf33b3af19b71846ca87411da016f4f0d
kseniya <kseniyamudrakovadev@gmail.com>
2022-01-16 23:28:05 +0100

    change controller user

--- backend/Controllers/user.controllers.js
+++ backend/Controllers/user.controllers.js
@@ -3,30 +3,39 @@ const {
     Users
 } = require("../models");
 
+/* --- Partie register --- */
+
 exports.register = async (req, res) => {
     const {
         username,
         useremail,
         userpassword
     } = req.body;
-    bcrypt.hash(userpassword, 10).then((hash) => {
-        Users.create({
+    // on appelle la fun (async) de hachage + demande "saler" pass 10 fois; hash crypté du pass
+    bcrypt.hash(userpassword, 10)
+        .then(hash => {
+            // création de l'User 
+            const user = Users.create({
                 username: username,
                 useremail: useremail,
-                userpassword: hash,
-            })
-            .then(() => {
-                res.json("Enregistré");
-            })
-            .catch((err) => {
-                if (err) {
-                    res.status(400).json({
-                        error: err,
-                    });
-                }
+                userpassword: hash
             });
-    });
+            user.save()
+                .then(() => res.status(201).json({
+                    message: 'Compte utilisateur créé !'
+                }))
+
+                .catch(error => res.status(400).json({
+                    error
+                }));
+        })
+        .catch(error => res.status(500).json({
+            error
+        }));
 };
+
+
+/* --- Partie login --- */
 
 exports.login = async (req, res) => {
     const {
@@ -42,12 +51,13 @@ exports.login = async (req, res) => {
 
     const user = await Users.findOne({
             where: {
-                username: username
+                username: req.body.username
             }
-        }).then(user => {
+        })
+        .then(user => {
             if (!user) {
                 return res.status(401).json({
-                    error: 'Utilisateur non trouvé !'
+                    error: 'Utilisateur introuvable !'
                 });
             }
             bcrypt.compare(req.body.userpassword, user.userpassword)
@@ -58,7 +68,8 @@ exports.login = async (req, res) => {
                         });
                     }
                     res.status(200).json({
-                        userId: user.id
+                        userId: user._id,
+                        token: 'TOKEN'
                     });
                 })
                 .catch(error => res.status(500).json({
@@ -68,81 +79,4 @@ exports.login = async (req, res) => {
         .catch(error => res.status(500).json({
             error
         }));
-};
-
-// Profile
-
-exports.getProfile = (req, res, next) => {
-    Users.findOne({
-            attributes: ["id", "useremail", "username", "isAdmin"], // éléments à afficher
-            where: {
-                id: id
-            },
-        })
-        .then((user) => {
-            console.log("User");
-            res.status(200).json(user);
-        })
-        .catch((err) => {
-            console.log("utilisateur non trouvé")
-            res.status(500).json(err)
-        });
-};
-
-// Gestion suppression
-
-exports.deleteProfile = (req, res) => {
-    //récupération de l'id de l'user dans le token
-    let userId = req.body.id;
-    if (userId != null) {
-        models.Users.findOne({
-            where: {
-                id: userId
-            }
-        }).then((user) => {
-            if (user != null) {
-                //si l'utilisateur a était correctement identifié
-                models.Message.findAll({
-                        where: {
-                            userId: user.id
-                        }
-                    }).then((posts) => {
-                        postMessage.forEach((posts) => {
-                            const messageId = message.id;
-                            models.Comment.destroy({
-                                where: {
-                                    messageId: messageId
-                                }
-                            });
-                        });
-                    }),
-
-                    models.Posts.destroy({
-                        //suppression de tous les posts de l'User
-                        where: {
-                            userId: user.id
-                        }
-                    })
-                    .then(() => {
-                        //Suppression de l'utilisateur
-                        models.Users.destroy({
-                                where: {
-                                    id: user.id
-                                },
-                            })
-                            .then(() => res.end())
-                            .catch((error) => console.log(error));
-                    })
-                    .catch((err) => res.status(500).json(err));
-            } else {
-                res.status(401).json({
-                    error: "Le compte est introuvable"
-                });
-            }
-        });
-    } else {
-        res.status(500).json({
-            error: "Suppression impossible",
-        });
-    }
 };
