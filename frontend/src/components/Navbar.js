f79a89187fefdc2f31a8897c46bab3f7f12e8863
Kseniya <kseniyamudrakovadev@gmail.com>
2022-01-27 14:36:11 +0100

    add responsive navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components.css";
+import "../components/Nav.css";
 import { NavLink } from "react-router-dom";
 import { UidContext } from "./AppContext.jsx";
 import Logout from "../Assets/logout.svg";
@@ -12,20 +12,29 @@ import { Container, Row, Col } from "@nextui-org/react"; */
 
 function Navbar() {
   return (
-    <nav className="Navbar">
-      <a className="NavbarLink" href="/">
-        Accueil{" "}
-      </a>{" "}
-      <a className="NavbarLink" href="/user/register">
-        S 'inscrire{" "}
-      </a>{" "}
-      <a className="NavbarLink" href="/user/login">
-        Se connecter{" "}
-      </a>{" "}
-      <div className="logopicture">
-        <img src={Logo} alt="Groupomania logo" className="logo" />
-      </div>{" "}
-    </nav>
+    <header className="header">
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-header">
+          <img src={Logo} alt="logo" className="logo" />
+        </div>
+        <div className="nav-btn">
+          <label htmlFor="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
+        <div className="nav-links">
+          <a href="/" target="_blank">
+            Accueil
+          </a>
+          <a href="/user/register">S'inscrire</a>
+          <a href="/user/login">Se connecter</a>
+        </div>
+      </div>
+    </header>
   );
 }
 
