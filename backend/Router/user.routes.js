9bcfc1b50704032afdc11e6eb5ff72f90e6a4395
Kseniya <kseniyamudrakovadev@gmail.com>
2022-01-28 21:10:09 +0100

    updates routes

--- backend/Router/user.routes.js
+++ backend/Router/user.routes.js
@@ -1,39 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const rateLimit = require("express-rate-limit");
 const auth = require("../middleware/auth");
-const userCtrl = require('../Controllers/user.controllers');
-const multer = require('../middleware/multer-config');
+const userCtrl = require("../Controllers/user.controllers");
 
-const fs = require("fs");
-const {
-    promisify
-} = require("util");
-const pipeline = promisify(require("stream").pipeline);
-
-
-// Limitation tentatives Register
-const createAccountLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1h window
-    max: 50, // blocage après 5 tentatives
-    message: "Too many accounts created from this IP, please try again after an hour",
-});
-
-// Partie sécurité login - limiter nbr tentatives Login
-const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-});
-
-router.post('/register', createAccountLimiter, userCtrl.register); // register
-router.post('/login', apiLimiter, userCtrl.login); // login
-
-router.put('/modify', userCtrl.modifyAccount); // Modif info User
-router.get('/profile', auth, userCtrl.getOneUser); // afficher un seul User
-router.get('/wall', auth, userCtrl.getAllUsers); // afficher tous les Users
-router.get('/:id', auth, userCtrl.deleteUser); // Suppression du compte User
-
-
-/* router.post('/upload', postCtr.upload); */
+router.post("/register", userCtrl.register); // création User
+router.post("/login", userCtrl.login); // Login
+router.get("/:id", auth, userCtrl.getOneUser); // Affichage un seul user
+router.put("/:id", auth, userCtrl.modifyUser); // Update compte User
+router.delete("/:id", auth, userCtrl.deleteUser); // suppresion compte User
+router.get("/users", auth, userCtrl.getAllUsers); // Affichage tous les comptes
 
 module.exports = router;
