5105586b3e5d1da2e8b946765ee20e3bc66962c3
Kseniya <kseniyamudrakovadev@gmail.com>
2022-07-06 16:08:39 +0200

    correction error, downgrade to react 17

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -1,11 +1,10 @@
+// Import toutes les pages
 import Dashboard from "./Dashboard";
 import Home from "./Home";
 import MissingPage from "./MissingPage";
 import ProfilePage from "./ProfilePage";
 import MembersPage from "./MembersPage";
 import PostPage from "./PostPage";
-
-
 
 export {
     Home,
